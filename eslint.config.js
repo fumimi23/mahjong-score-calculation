ab162207895cc73ed74b25c688ef74801e1bae47
fumimi23 <highspec.nite@gmail.com>
2024-08-18 00:44:10 +0900

    chore: ESLint Stylistic導入

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,13 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.yarn'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -25,4 +26,5 @@ export default tseslint.config(
       ],
     },
   },
+  stylistic.configs['recommended-flat'],
 )
